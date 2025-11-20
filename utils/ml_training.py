cc5191e87990d7bf24633c3129a65e260d4b4358
Phyke <prombot.cherdchoo@gmail.com>
2025-11-20 15:05:34 +0700

    Finish Training

--- utils/ml_training.py
+++ utils/ml_training.py
@@ -28,22 +28,21 @@ def shuffle_indexes(
     return X_train, y_train
 
 
-def get_model_size_joblib(model: object, filename: str = "tmp_model.joblib") -> float:
+def save_and_get_model_size_joblib(model: object, filename: str) -> float:
+    Path(filename).parent.mkdir(parents=True, exist_ok=True)
     # Save model
     joblib.dump(model, filename, compress=("lzma", 3))
     # Measure size using pathlib
-    size_mb = Path(filename).stat().st_size / (1024**2)  # in MB
-    # Remove temp file
-    Path(filename).unlink()
-    return size_mb
+    return Path(filename).stat().st_size / (1024**2)  # in MB
 
 
-def run_ml_experiments(
+def run_ml_experiments(  # noqa: PLR0913
     X_train: FeatureMatrix_float32,
     y_train: FeatureMatrix_float32,
     X_test: NDArray[np.uint8],
     y_test: NDArray[np.uint8],
     path_output: Path,
+    feature_set_name: str,
 ) -> dict[str, dict]:
     models = {
         "GaussianNB": GaussianNB(),
@@ -56,12 +55,15 @@ def run_ml_experiments(
     results = {}
     for name, model in models.items():
         # Train
-        start = time.time()
+        start_train = time.time()
         model.fit(X_train, y_train)
-        end = time.time()
-        total_time = end - start
+        end_train = time.time()
+        total_time_train = end_train - start_train
         # Predict / metrics
+        start_pred = time.time()
         y_pred = model.predict(X_test)
+        end_pred = time.time()
+        total_time_pred = end_pred - start_pred
         report_str = classification_report(
             y_test,
             y_pred,
@@ -83,19 +85,22 @@ def run_ml_experiments(
         )
 
         # Model size (joblib)
-        size_mb = get_model_size_joblib(model)
-        print(f"\n{name}:")
-        print(f"Training time: {total_time:.2f} seconds")
-        print(report_str)
+        size_mb = save_and_get_model_size_joblib(model, f"outputs/models/{feature_set_name}/{name}.joblib")
+        print("\n--------------------------------------------------")
+        print(f"{name}:")
+        print(f"Training time: {total_time_train:.2f} seconds for {len(X_train)} samples")
+        print(f"Prediction time: {total_time_pred:.2f} seconds for {len(X_test)} samples")
         print(f"Model size (joblib): {size_mb:.3f} MB")
+        print(report_str)
         results[name] = {
-            "training_time": total_time,
+            "training_time": total_time_train,
+            "prediction_time": total_time_pred,
             "report": report,
             "report_str": report_str,
             "model_size_mb": size_mb,
         }
 
-    with path_output.open("w") as f:
+    with (path_output / "models" / feature_set_name / "metrics.json").open("w") as f:
         json.dump(results, f, indent=4)
 
     return results
@@ -109,7 +114,8 @@ def extract_metrics(results: dict) -> tuple:
     f1 = [results[m]["report"]["macro avg"]["f1-score"] for m in models]
     training_time = [results[m]["training_time"] for m in models]
     model_sizes = [results[m]["model_size_mb"] for m in models]
-    return models, accuracy, precision, recall, f1, training_time, model_sizes
+    prediction_time = [results[m]["prediction_time"] for m in models]
+    return models, accuracy, precision, recall, f1, training_time, model_sizes, prediction_time
 
 
 def plot_classification_metrics(results: dict) -> None:
@@ -131,11 +137,12 @@ def plot_classification_metrics(results: dict) -> None:
     plt.show()
 
 
-def plot_training_times(results: dict) -> None:
-    models, *_, training_time, __ = extract_metrics(results)
+def plot_training_and_prediction_times(results: dict) -> None:
+    models, *_, training_time, prediction_time = extract_metrics(results)
 
     plt.figure(figsize=(10, 5))
     plt.bar(models, training_time)
+    plt.bar(models, prediction_time, bottom=training_time)
     plt.xticks(rotation=45)
     plt.ylabel("Training Time (seconds)")
     plt.title("Model Training Time Comparison")
